b002035d15275fe30a7c99435a5e30279834be66
RL31 <remilardellier@hotmail.fr>
2022-01-06 21:39:34 +0100

    fin q_velo

--- programmes R/01_exploration_q_velo.R
+++ programmes R/01_exploration_q_velo.R
@@ -1,8 +1,8 @@
 library(tidyverse)
 
 base_equipement_velo <- read.csv("donnees/q_velo_public.csv",
-                                  sep=";",
-                                  colClasses = c("pond_velo"="numeric","IDENT_MEN"="character"))
+                                 sep=";",
+                                 colClasses = c("pond_velo"="numeric","IDENT_MEN"="character"))
 
 # Répartition des types de vélo
 base_equipement_velo %>% 
@@ -27,7 +27,7 @@ base_equipement_velo %>%
        caption="Source : Sdes, Enquête mobilité des personnes 2019\nTraitements et erreurs : @Re_Mi_La")+
   theme_minimal()+
   theme(text = element_text(size=10),
-    plot.caption = element_text(face="italic",size=7))
+        plot.caption = element_text(face="italic",size=7))
 
 
 ggsave("sorties/graphique_type_velo.jpeg",bg="white")
@@ -55,10 +55,10 @@ base_equipement_velo %>%
                     labels=c("FALSE"="pour se promener ou faire du sport",
                              "TRUE"="pour des déplacements \"utilitaires\""),
                     values = viridis::viridis(2,direction = -1 )
-                    )+
-    coord_flip()+
+  )+
+  coord_flip()+
   labs(title="Vélo-loisir ou vélo-utilitaire ? ",
-  subtitle="Un équipement prévu et surtout utilisé pour se promener et faire du sport",
+       subtitle="Un équipement prévu et surtout utilisé pour se promener et faire du sport",
        y="Part dans les vélos d’adultes utilisés\ndans les 12 mois précédant l’enquête (%)",
        caption="Source : Sdes, Enquête mobilité des personnes 2019\nTraitements et erreurs : @Re_Mi_La")+
   theme_minimal()+
@@ -71,7 +71,6 @@ ggsave("sorties/graphique_type_et_usage.jpeg",bg="white",
        width = 7,height = 5)
 
 # Qui utilise quoi ? (ou plutôt quoi est utilisé par qui ? cf pondération)
-
 base_indiv <- read.csv("donnees/tcm_ind_public.csv",
                        sep=";",
                        colClasses = c("ident_ind"="character","ident_men"="character"))
@@ -94,15 +93,14 @@ base_equipement_velo %>%
                              "8"="Tandem [tous types]",
                              "9"="Vélo cargo ou triporteur",
                              "99"="Autre"))+
-   scale_fill_manual(name="Personne utilisant le vélo",
-                     labels=c("Homme",
-                              "Femme",
-                              "Non précisé"),
-                     values = viridis::viridis(3,direction = -1 )
-   )+
+  scale_fill_manual(name="Personne utilisant le vélo",
+                    labels=c("Homme",
+                             "Femme",
+                             "Non précisé"),
+                    values = viridis::viridis(3,direction = -1 )
+  )+
   coord_flip()+
-  labs(title="Vélo-loisir ou vélo-utilitaire ? ",
-       subtitle="Un équipement prévu et surtout utilisé pour se promener et faire du sport",
+  labs(title="Les vélos \"sportifs\" sont surtout utilisés par des hommes ",
        y="Part dans les vélos d’adultes utilisés\ndans les 12 mois précédant l’enquête (%)",
        caption="Source : Sdes, Enquête mobilité des personnes 2019\nTraitements et erreurs : @Re_Mi_La")+
   theme_minimal()+
@@ -110,10 +108,41 @@ base_equipement_velo %>%
         plot.caption = element_text(face="italic",size=7),
         legend.position = "bottom")+
   guides(fill=guide_legend(nrow = 3, byrow = TRUE) )
-  
+
+ggsave("sorties/graphique_type_sexe.jpeg",bg="white",
+       width = 7,height = 5)  
 
 
+# Zoom sur les vélos équipés d'un siege enfant
+base_men <- read.csv("donnees/tcm_men_public.csv",
+                       sep=";",
+                       colClasses = c("ident_men"="character"))
 
+base_indiv %>% count(tr_age)
+base_equipement_velo %>% 
+  mutate(ident_ind = paste0(IDENT_MEN,str_pad(KVELOQUI,2,"left","0"))) %>% 
+  left_join(base_indiv,by=c("ident_ind")) %>% 
+  left_join(base_men,by=c("IDENT_MEN"="ident_men")) %>% 
+  filter(NENFANTS>0 & tr_age =="De 25 à 34 ans") %>% 
+  count(SEXE,KVELOBTP,wt=pond_velo) %>% 
+  group_by(SEXE) %>% 
+  mutate(pct=n/sum(n)*100) %>% 
+  filter(KVELOBTP==1) %>% 
+  ggplot(aes(x=reorder(as.factor(SEXE),(pct)),y=pct))+
+  geom_bar(stat="identity",fill=viridis::viridis(1,direction = 1 ))+
+  scale_x_discrete(name="",
+                    labels=c("1"="Vélo principalement\nutilisé par un homme",
+                             "2"="Vélo principalement\nutilisé par une femme"))+
+  coord_flip()+
+  labs(title="Un tiers des vélos des femmes* sont équipés pour transporter un enfant",
+       subtitle = "* femmes de 25 à 34 ans ayant au moins 1 enfant",
+       y="Part dans les vélos (%)",
+       caption="Source : Sdes, Enquête mobilité des personnes 2019\nTraitements et erreurs : @Re_Mi_La")+
+  theme_minimal()+
+  theme(text = element_text(size=10),
+        plot.caption = element_text(face="italic",size=7),
+        legend.position = "bottom")+
+  guides(fill=guide_legend(nrow = 3, byrow = TRUE) )
 
-KVKM1SEM	Num		Nb km parcourus dernière semaine (s'il est utilisé au moins deux ou trois fois par mois)
-
+ggsave("sorties/graphique_velos_enfants.jpeg", bg="white",
+       width=7,height = 5)
